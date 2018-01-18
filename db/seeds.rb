893ccb431d970604b40e5cdb1a5077f33dfe0959
LouisMarie Bonnefont <louis_marie.bonnefont@edu.escpeurope.eu>
2018-01-18 23:42:32 +0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+Bet.destroy_all
 Player.destroy_all
 Bettor.destroy_all
 Game.destroy_all
