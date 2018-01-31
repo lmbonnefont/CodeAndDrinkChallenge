896a2cf8a0143a234aeb6dea2157b0d6ca80854f
LouisMarie Bonnefont <louis_marie.bonnefont@edu.escpeurope.eu>
2018-01-31 22:34:19 +0500

    adding security on bets

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -1,25 +1,34 @@
 class BetsController < ApplicationController
   def new
-    @bet = Bet.new
-    @game = Game.find(params[:game_id])
   end
 
   def create
-    player = Player.find(bet_params["player"])
-    bettor = Bettor.find(bet_params["bettor"])
-    shot = bet_params["shot"].to_i
-
-    @game = Game.find(params[:game_id])
 
     bet = Bet.new
-    bet.player = player
-    bet.bettor = bettor
+
+    player = bet_params["player"]
+    if player != ""
+      player = Player.find(player)
+      bet.player = player
+    end
+
+    bettor = bet_params["bettor"]
+    if bettor != ""
+      bettor = Bettor.find(bettor)
+      bet.bettor = bettor
+    end
+
+    shot = bet_params["shot"].to_i
     bet.shot = shot
+
+    @game = Game.find(params[:game_id])
     bet.game_id = @game.id
-    bet.save!
 
-
-    redirect_to user_game_path(current_user, @game)
+    if bet.save
+      redirect_to user_game_path(current_user, @game)
+    else
+      render :new
+    end
 
   end
 
